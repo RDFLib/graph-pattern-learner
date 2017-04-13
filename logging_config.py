42f62a9f8db81e939a048936e8c7ac9eb0591594
Joern Hees <dev@joernhees.de>
2017-04-13 17:44:01 +0200

    handle creation of log dir more graceful with scoop workers in mind

--- logging_config.py
+++ logging_config.py
@@ -104,8 +104,10 @@ class CompressedRotatingFileHandler(logging.handlers.RotatingFileHandler):
 
 # set up logging (keep multiprocessing in mind):
 LOG_DIR = os.getenv('GP_LEARNER_LOG_DIR', 'logs')
-if not os.path.exists(LOG_DIR):
+try:
     os.mkdir(LOG_DIR)
+except OSError:
+    pass
 LOG_SUFFIX = '.log'
 START_TIME = time.strftime('%Y-%m-%dT%H-%M-%S')
 pid = os.getpid()
